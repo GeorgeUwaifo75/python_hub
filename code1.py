644e38c8f2a270aa9579b9b7007e7ca26a15e7c8
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-05-19 14:56:02 +0100

    Update code1.py

--- code1.py
+++ code1.py
@@ -5,15 +5,14 @@ import numpy as np
 st.title("Code test hub")
 
 # Set up the plot
-#xpoints = np.array([0, 6])
-#ypoints = np.array([0, 250])
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
 
 #xpoints = np.array([1, 2, 6, 8])
-ypoints = np.array([3, 8, 1, 10])
+#ypoints = np.array([3, 8, 1, 10])
 
 fig, ax = plt.subplots()
-#ax.plot(xpoints, ypoints)
-ax.plot(ypoints)
+ax.plot(xpoints, ypoints)
 
 # Display the plot in Streamlit
 st.pyplot(fig)
