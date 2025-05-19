f0474283690d90e2239d8e1a31a3b57313c9c672
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-05-19 14:43:14 +0100

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,11 +1,18 @@
+import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-import streamlit as st
 
 st.title("Code test hub")
 
+# Set up the plot
 xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 
-plt.plot(xpoints, ypoints)
-plt.show()
+fig, ax = plt.subplots()
+ax.plot(xpoints, ypoints)
+
+# Display the plot in Streamlit
+st.pyplot(fig)
+
+# Optional: Add a title
+st.title('Simple Line Plot')
