fe551fd01aca36bf4171c256a1e2784582a66764
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2025-05-19 14:33:54 +0100

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,6 +1,12 @@
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import streamlit as st
 
 st.title("Code test hub")
-st.write("Hello Now")
 st.write("Version:", matplotlib.__version__)
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
